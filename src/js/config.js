1cb9bfb4433a63f603c8a5a7380fe21fba387bfe
Jarosław Goryca <jagor103@gmail.com>
2024-10-30 15:53:15 +0100

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'fb741cf5-d5fc-4444-90fd-e7ee8bdbb2d3';
+export const MODAL_CLOSE_SEC = 2.5;
